1736af23252dd314f3308ad3a4f185b44db6f7c6
m-a-u-d <maudfreeman@gmail.com>
2022-07-10 20:19:52 +1000

    b

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ import torch
 import pyttsx3
 import cv2
 import time
+import os
 
 #voice engine init
+
 engine = pyttsx3.init() # object creation
+#voices = engine.getProperty('voices')
+#print(voices)
 
 
-model = VisionEncoderDecoderModel.from_pretrained("vit-gpt2-image-captioning")
-feature_extractor = ViTFeatureExtractor.from_pretrained("vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("vit-gpt2-image-captioning")
+model = VisionEncoderDecoderModel.from_pretrained("model/vit-gpt2-image-captioning")
+feature_extractor = ViTFeatureExtractor.from_pretrained("model/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("model/vit-gpt2-image-captioning")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
@@ -37,24 +41,21 @@ def predict_step(image_paths):
   output_ids = model.generate(pixel_values, **gen_kwargs)
 
   preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+  print(preds)
   preds = [pred.strip() for pred in preds]
   return preds
 
-
+camera = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
 while True:
-  cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
-  ret,frame = cap.read() # return a single frame in variable `frame`
-  cv2.imshow('img1',frame) #display the captured image
-  cv2.imwrite('c1.png',frame)
-
-        
-
-  cap.release()
-
-  a = predict_step(['c1.png']) # ['a woman in a hospital bed with a woman in a hospital bed']
-  print(a)
-  engine.say(a)
+    
+  ret,frame = camera.read() # return a single frame in variable `frame`
+  #cv2.imshow('img1',frame) #display the captured image
+  cv2.imwrite('frame.png',frame) # 
+  caption = predict_step(['frame.png'])[0] # ['a woman in a hospital bed with a woman in a hospital bed']
+  os.remove("frame.png")
+  print(caption)
+  engine.say(caption)
   engine.runAndWait()
   
-  time.sleep(15)
-  cv2.destroyAllWindows()
+  time.sleep(1)
+  #cv2.destroyAllWindows()
